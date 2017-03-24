42c320bd2194c36ea76b6b9de1c55dc25726f1db
shma <id.shma@gmail.com>
2017-03-24 23:47:45 +0900

    404ページを追加

--- app.js
+++ app.js
@@ -36,8 +36,7 @@ app.use('/users', users);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.status(404).render('error_404', {  title: '浅野鍛冶屋' });
 });
 
 // error handler
