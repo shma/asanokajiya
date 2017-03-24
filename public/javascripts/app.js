0d2749c026a4e1a378d4455482743db5767169ca
shma <id.shma@gmail.com>
2017-03-24 23:10:10 +0900

    画像の遅延ロードをトライ

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,89 +1,94 @@
 require('../../node_modules/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js')
 require('../../node_modules/scrollmagic/scrollmagic/uncompressed/plugins/animation.velocity.js')
 require('../../node_modules/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
-import $ from 'jquery'
+window.jQuery = window.$ = require('jquery');
+require('jquery-lazyload')
+
+
 import scrollMagic from 'scrollmagic'
 require('../../node_modules/gsap/ScrollToPlugin.js')
 
+
 "use strict";
 
+
 $(() => {
-// init controller
-var controller = new scrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+  $("img.lazy").lazyload();
+  var nav = $('#menu'),
+  hambtn = $('#mobileHead'),
+  offset = nav.offset();
+  // init controller
+  var controller = new scrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
 
-new scrollMagic.Scene({triggerElement: "#katana"})
-      .setTween("#katana > div", {y: "100%", ease: Linear.easeNone})
-      .addTo(controller);
-
-new scrollMagic.Scene({triggerElement: "#samurai"})
-      .setTween("#samurai > div", {y: "100%", ease: Linear.easeNone})
-      .addTo(controller);
-
-new scrollMagic.Scene({triggerElement: "#workshop"})
-      .setTween("#workshop > div", {y: "100%", ease: Linear.easeNone})
-      .addTo(controller);
-new scrollMagic.Scene({triggerElement: "#about"})
-      .setTween("#about > div", {y: "100%", ease: Linear.easeNone})
-      .addTo(controller);
-new scrollMagic.Scene({triggerElement: "#contact"})
-      .setTween("#contact > div", {y: "100%", ease: Linear.easeNone})
-      .addTo(controller)
-
-var nav = $('#menu'),
-hambtn = $('#mobileHead'),
-offset = nav.offset();
-if (nav[0]) {
-  $(window).scroll(function () {
-    if($(window).scrollTop() > offset.top) {
-      hambtn.addClass('fixed');
-      nav.addClass('fixed');
-    } else {
-      hambtn.removeClass('fixed');
-      nav.removeClass('fixed');
-    }
-  });
-}
+  if (nav[0]) {
+    new scrollMagic.Scene({triggerElement: "#katana"})
+          .setTween("#katana > div", {y: "100%", ease: Linear.easeNone})
+          .addTo(controller);
 
 
-var detailNav = $('#dMenu'),
-detailLogo = $('.detail-logo'),
-detailOffset = detailNav.offset();
-if (detailNav[0]) {
-  $(window).scroll(function () {
-    console.log(detailNav[0]);
-    if($(window).scrollTop() > detailOffset.top) {
-      detailNav.addClass('fixed');
-      hambtn.addClass('fixed');
-      hambtn.addClass('black');
-      detailLogo.addClass('fixedBlock');
-      detailNav.addClass('black');
-    } else {
-      detailLogo.removeClass('fixedBlock');
-      hambtn.removeClass('fixed');
-      detailNav.removeClass('black');
-      detailNav.removeClass('fixed');
-    }
-  });
-}
+    new scrollMagic.Scene({triggerElement: "#samurai"})
+          .setTween("#samurai > div", {y: "100%", ease: Linear.easeNone})
+          .addTo(controller);
 
-var activeClass = 'active';
-function hamburgBtn() {
-    $('.btn_hamburger').on('click', function(event) {
-        var button = $(this);
-        event.preventDefault();
+    new scrollMagic.Scene({triggerElement: "#workshop"})
+          .setTween("#workshop > div", {y: "100%", ease: Linear.easeNone})
+          .addTo(controller);
+    new scrollMagic.Scene({triggerElement: "#about"})
+          .setTween("#about > div", {y: "100%", ease: Linear.easeNone})
+          .addTo(controller);
+    new scrollMagic.Scene({triggerElement: "#contact"})
+          .setTween("#contact > div", {y: "100%", ease: Linear.easeNone})
+          .addTo(controller)
 
-        button.find('a').toggleClass(activeClass);
-        button.next().slideToggle(250);
+
+    $(window).scroll(function () {
+      if($(window).scrollTop() > offset.top) {
+        hambtn.addClass('fixed');
+        nav.addClass('fixed');
+      } else {
+        hambtn.removeClass('fixed');
+        nav.removeClass('fixed');
+      }
     });
-};
-hamburgBtn();
+  }
 
+  var detailNav = $('#dMenu'),
+  detailLogo = $('.detail-logo'),
+  detailOffset = detailNav.offset();
+  if (detailNav[0]) {
+    $(window).scroll(function () {
+      if($(window).scrollTop() > detailOffset.top) {
+        detailNav.addClass('fixed');
+        hambtn.addClass('fixed');
+        hambtn.addClass('black');
+        detailLogo.addClass('fixedBlock');
+        detailNav.addClass('black');
+      } else {
+        detailLogo.removeClass('fixedBlock');
+        hambtn.removeClass('fixed');
+        detailNav.removeClass('black');
+        detailNav.removeClass('fixed');
+      }
+    });
+  }
 
-var tween = TweenMax.from("#animate", 0.5, {autoAlpha: 0, scale: 0.7});
+  var activeClass = 'active';
+  function hamburgBtn() {
+      $('.btn_hamburger').on('click', function(event) {
+          var button = $(this);
+          event.preventDefault();
 
-controller.scrollTo(function (newpos) {
-  TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
-});
+          button.find('a').toggleClass(activeClass);
+          button.next().slideToggle(250);
+      });
+  };
+  hamburgBtn();
+
+  var tween = TweenMax.from("#animate", 0.5, {autoAlpha: 0, scale: 0.7});
+
+  controller.scrollTo(function (newpos) {
+    TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
+  });
 
   //  bind scroll to anchor links
   $('.contact').on("click", function (e) {
