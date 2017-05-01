beb4c09c37262feca69e16706dce1d49e1ba705b
Matsuno Shunya <shma@MatsunonoMacBook-Pro.local>
2017-05-01 22:16:52 +0900

    ロゴ変更

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -12,100 +12,101 @@ require('../../node_modules/gsap/ScrollToPlugin.js')
 
 
 $(() => {
-  $("img.lazy").lazyload({
-    threshold: 200,
-	  effect: "fadeIn",
-  });
-
-  var nav = $('#menu'),
-  hambtn = $('#mobileHead'),
-  offset = nav.offset();
-  // init controller
-  var controller = new scrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
-
-  if (nav[0]) {
-    new scrollMagic.Scene({triggerElement: "#katana"})
-          .setTween("#katana > div", {y: "100%", ease: Linear.easeNone})
-          .addTo(controller);
-
-
-    new scrollMagic.Scene({triggerElement: "#samurai"})
-          .setTween("#samurai > div", {y: "100%", ease: Linear.easeNone})
-          .addTo(controller);
-
-    new scrollMagic.Scene({triggerElement: "#workshop"})
-          .setTween("#workshop > div", {y: "100%", ease: Linear.easeNone})
-          .addTo(controller);
-    new scrollMagic.Scene({triggerElement: "#about"})
-          .setTween("#about > div", {y: "100%", ease: Linear.easeNone})
-          .addTo(controller);
-    new scrollMagic.Scene({triggerElement: "#contact"})
-          .setTween("#contact > div", {y: "100%", ease: Linear.easeNone})
-          .addTo(controller)
-
-
-    $(window).scroll(function () {
-      if($(window).scrollTop() > offset.top) {
-        hambtn.addClass('fixed');
-        nav.addClass('fixed');
-      } else {
-        hambtn.removeClass('fixed');
-        nav.removeClass('fixed');
-      }
+    $("img.lazy").lazyload({
+        threshold: 200,
+        effect: "fadeIn",
     });
-  }
 
-  var detailNav = $('#dMenu'),
-  detailLogo = $('.detail-logo'),
-  detailOffset = detailNav.offset();
-  if (detailNav[0]) {
-    $(window).scroll(function () {
-      if($(window).scrollTop() > detailOffset.top) {
-        detailNav.addClass('fixed');
-        hambtn.addClass('fixed');
-        hambtn.addClass('black');
-        detailLogo.addClass('fixedBlock');
-        detailNav.addClass('black');
-      } else {
-        detailLogo.removeClass('fixedBlock');
-        hambtn.removeClass('fixed');
-        detailNav.removeClass('black');
-        detailNav.removeClass('fixed');
-      }
-    });
-  }
+    var nav = $('#menu'),
+        hambtn = $('#mobileHead'),
+        offset = nav.offset();
+    // init controller
+    var controller = new scrollMagic.Controller({ globalSceneOptions: { triggerHook: "onEnter", duration: "200%" } });
 
-  var activeClass = 'active';
-  function hamburgBtn() {
-      $('.btn_hamburger').on('click', function(event) {
-          var button = $(this);
-          event.preventDefault();
+    if (nav[0]) {
+        new scrollMagic.Scene({ triggerElement: "#katana" })
+            .setTween("#katana > div", { y: "100%", ease: Linear.easeNone })
+            .addTo(controller);
 
-          button.find('a').toggleClass(activeClass);
-          button.next().slideToggle(250);
-      });
-  };
-  hamburgBtn();
 
-  var tween = TweenMax.from("#animate", 0.5, {autoAlpha: 0, scale: 0.7});
+        new scrollMagic.Scene({ triggerElement: "#samurai" })
+            .setTween("#samurai > div", { y: "100%", ease: Linear.easeNone })
+            .addTo(controller);
 
-  controller.scrollTo(function (newpos) {
-    TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
-  });
+        new scrollMagic.Scene({ triggerElement: "#workshop" })
+            .setTween("#workshop > div", { y: "100%", ease: Linear.easeNone })
+            .addTo(controller);
+        new scrollMagic.Scene({ triggerElement: "#about" })
+            .setTween("#about > div", { y: "100%", ease: Linear.easeNone })
+            .addTo(controller);
+        new scrollMagic.Scene({ triggerElement: "#contact" })
+            .setTween("#contact > div", { y: "100%", ease: Linear.easeNone })
+            .addTo(controller)
 
-  //  bind scroll to anchor links
-  $('.contact').on("click", function (e) {
-    var id = $(this).attr("href");
-    if ($(id).length > 0) {
-      e.preventDefault();
 
-      // trigger scroll
-      controller.scrollTo(id);
-
-      // if supported by the browser we can even update the URL.
-      if (window.history && window.history.pushState) {
-        history.pushState("", document.title, id);
-      }
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > offset.top) {
+                hambtn.addClass('fixed');
+                nav.addClass('fixed');
+            } else {
+                hambtn.removeClass('fixed');
+                nav.removeClass('fixed');
+            }
+        });
     }
-  });
+
+    var detailNav = $('#dMenu'),
+        detailLogo = $('.detail-logo'),
+        detailOffset = detailNav.offset();
+    if (detailNav[0]) {
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > detailOffset.top) {
+                detailNav.addClass('fixed');
+                hambtn.addClass('fixed');
+                hambtn.addClass('black');
+                detailLogo.addClass('fixedBlock');
+                detailNav.addClass('black');
+            } else {
+                detailLogo.removeClass('fixedBlock');
+                hambtn.removeClass('fixed');
+                detailNav.removeClass('black');
+                detailNav.removeClass('fixed');
+            }
+        });
+    }
+
+    var activeClass = 'active';
+
+    function hamburgBtn() {
+        $('.btn_hamburger').on('click', function(event) {
+            var button = $(this);
+            event.preventDefault();
+
+            button.find('a').toggleClass(activeClass);
+            button.next().slideToggle(250);
+        });
+    };
+    hamburgBtn();
+
+    var tween = TweenMax.from("#animate", 0.5, { autoAlpha: 0, scale: 0.7 });
+
+    controller.scrollTo(function(newpos) {
+        TweenMax.to(window, 0.5, { scrollTo: { y: newpos } });
+    });
+
+    //  bind scroll to anchor links
+    $('.contact').on("click", function(e) {
+        var id = $(this).attr("href");
+        if ($(id).length > 0) {
+            e.preventDefault();
+
+            // trigger scroll
+            controller.scrollTo(id);
+
+            // if supported by the browser we can even update the URL.
+            if (window.history && window.history.pushState) {
+                history.pushState("", document.title, id);
+            }
+        }
+    });
 });
